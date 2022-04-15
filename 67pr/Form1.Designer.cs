3dc3a11ae2a1a7b3de3ff937470da584b44dfd04
HellGoesOn <35145123+HellGoesOn@users.noreply.github.com>
2022-04-15 15:08:47 +0300

    Добавьте файлы проекта.

---
+++ 67pr/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace _67pr
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.vivod1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.vivod2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(72, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "6";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // vivod1
+            // 
+            this.vivod1.Location = new System.Drawing.Point(12, 41);
+            this.vivod1.Name = "vivod1";
+            this.vivod1.Size = new System.Drawing.Size(211, 196);
+            this.vivod1.TabIndex = 1;
+            this.vivod1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(307, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "7";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // vivod2
+            // 
+            this.vivod2.Location = new System.Drawing.Point(296, 41);
+            this.vivod2.Name = "vivod2";
+            this.vivod2.Size = new System.Drawing.Size(198, 196);
+            this.vivod2.TabIndex = 3;
+            this.vivod2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.vivod2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.vivod1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox vivod1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox vivod2;
+    }
+}
---
+++ 67pr/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _67pr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                int novoeChislo = i;
+                SmenitZnak(ref novoeChislo);
+                vivod1.Text += novoeChislo + "\n";
+            }
+        }
+
+        public float Rast(float x1, float y1, float x2, float y2)
+        {
+            return (float)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+
+        public float Geron(float a, float b, float c)
+        {
+            float p = (a + b + c) / 2;
+            return (float)Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public void SmenitZnak(ref int chislo)
+        {
+            if (chislo % 2 == 0)
+                chislo *= -1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            float x1, x2, x3, x4, y1, y2, y3, y4;
+
+            x1 = 1;
+            y1 = 1;
+            
+            x2 = 3;
+            y2 = 1;
+
+            x3 = 1;
+            y3 = 4;
+
+            x4 = 2;
+            y4 = 4;
+
+            float s1 = Geron(Rast(x1, y1, x2, y2), Rast(x1, y1, x3, y3), Rast(x2, y2, x3, y3));
+            float s2 = Geron(Rast(x2, y2, x4, y4), Rast(x4, y4, x3, y3), Rast(x2, y2, x3, y3));
+
+            float min = Math.Min(s1, s2);
+
+            vivod2.Text += "Площадь 1=" + s1 + "\n";
+            vivod2.Text += "Площадь 2=" + s2 + "\n";
+            vivod2.Text += "Наименьшая плошадь=" + min + "\n";
+        }
+    }
+}
